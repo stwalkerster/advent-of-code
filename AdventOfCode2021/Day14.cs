e1743ccd7de1d427d58ae254b9380d629125070c
Simon Walker <github@stwalkerster.co.uk>
2023-10-31 00:40:51 +0000

    Day 14 part 2

--- AdventOfCode2021/Day14.cs
+++ AdventOfCode2021/Day14.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace AdventOfCode2021
 {
     using System;
@@ -5,6 +7,7 @@ namespace AdventOfCode2021
     using System.IO;
     using System.Linq;
     using System.Text;
+    using CC = System.Collections.Generic.Dictionary<char, ulong>;
 
     public class Day14
     {
@@ -46,42 +49,127 @@ namespace AdventOfCode2021
             Console.WriteLine(result);
         }
 
+        [DebuggerDisplay("{DebuggerDisplay,nq}")]
+        class InsertionRule
+        {
+            public char Left { get; set; }
+            public char Right { get; set; }
+            public char Result { get; set; }
+            
+            public InsertionRule LeftResult { get; set; }
+            public InsertionRule RightResult { get; set; }
+
+            public string DebuggerDisplay => $"{Left}{Right} -> {Result}";
+        }
+        
         public void Part2()
         {
             const int IterCount = 40;
-            var (template, insertionRules) = this.LoadData("Data/Day14/example.txt");
+            var (template, insertionRules) = this.LoadData("Data/Day14/main.txt");
 
-            for (var i = 1; i <= IterCount; i++)
+            var rules = insertionRules.ToDictionary(k => k.Key, x => new InsertionRule
             {
-                var sb = new StringBuilder();
-                for (var p = 1; p < template.Length; p++)
-                {
-                    sb.Append(template[p - 1]);
-                    sb.Append(insertionRules[template.Substring(p - 1, 2)]);
-                }
+                Left = x.Key[0],
+                Right = x.Key[1],
+                Result = x.Value[0]
+            });
 
-                sb.Append(template[^1]);
-                template = sb.ToString();
+            var globalCharCounts = new CC();
+            var charCountsCache = new Dictionary<string, CC>();
             
-                Console.WriteLine($"{i}: Length: {template.Length}");
+            foreach (var r in rules)
+            {
+                globalCharCounts.TryAdd(r.Value.Left, 0);
+                globalCharCounts.TryAdd(r.Value.Right, 0);
+                globalCharCounts.TryAdd(r.Value.Result, 0);
+
+                r.Value.LeftResult = rules[r.Value.Left.ToString() + r.Value.Result];
+                r.Value.RightResult = rules[r.Value.Result.ToString() + r.Value.Right];
             }
 
-            var frequency = template.ToCharArray()
-                .Aggregate(
-                    new Dictionary<char, ulong>(),
-                    (ints, c) =>
+            CC WalkTree(InsertionRule pair, int depth)
+            {
+                var charCounts = new CC { { pair.Result, 1 } };
+
+                if (depth >= IterCount)
+                {
+                    return charCounts;
+                }
+                
+                CC l;
+                var lKey = $"{pair.LeftResult.Left}{pair.LeftResult.Right}/{depth + 1}";
+                if (!charCountsCache.ContainsKey(lKey))
+                {
+                    l = WalkTree(pair.LeftResult, depth+1);
+                    charCountsCache.TryAdd(lKey, l);
+                }
+                else
+                {
+                    l = charCountsCache[lKey];
+                }
+                
+                CC r;
+                var rKey = $"{pair.RightResult.Left}{pair.RightResult.Right}/{depth + 1}";
+                if (!charCountsCache.ContainsKey(rKey))
+                {
+                    r = WalkTree(pair.RightResult, depth+1);
+                    charCountsCache.TryAdd(rKey, r);
+                }
+                else
+                {
+                    r = charCountsCache[rKey];
+                }
+
+                foreach (var kvp in l)
+                {
+                    if (charCounts.ContainsKey(kvp.Key))
                     {
-                        if (!ints.ContainsKey(c))
-                        {
-                            ints.Add(c, 0);
-                        }
+                        charCounts[kvp.Key] += kvp.Value;
+                    }
+                    else
+                    {
+                        charCounts.Add(kvp.Key, kvp.Value);
+                    }
+                }
+                
+                foreach (var kvp in r)
+                {
+                    if (charCounts.ContainsKey(kvp.Key))
+                    {
+                        charCounts[kvp.Key] += kvp.Value;
+                    }
+                    else
+                    {
+                        charCounts.Add(kvp.Key, kvp.Value);
+                    }
+                }
 
-                        ints[c]++;
-                        return ints;
-                    });
+                return charCounts;
+            }
 
-            var result = frequency.Max(x => x.Value) - frequency.Min(x => x.Value);
-            Console.WriteLine(result);
+            globalCharCounts[template[0]]++;
+            for (var i = 1; i < template.Length; i++)
+            {
+                Console.WriteLine($"[{DateTime.UtcNow.ToLongTimeString()}]: {i} / {template.Length -1}");
+                globalCharCounts[template[i]]++;
+                var result = WalkTree(rules[template.Substring(i-1, 2)], 1);
+                
+                foreach (var kvp in result)
+                {
+                    if (globalCharCounts.ContainsKey(kvp.Key))
+                    {
+                        globalCharCounts[kvp.Key] += kvp.Value;
+                    }
+                    else
+                    {
+                        globalCharCounts.Add(kvp.Key, kvp.Value);
+                    }
+                }
+            }
+            
+            Console.WriteLine(globalCharCounts.Values.Min());
+            Console.WriteLine(globalCharCounts.Values.Max());
+            Console.WriteLine(globalCharCounts.Values.Max() - globalCharCounts.Values.Min());
         }
         
         private (string Template, Dictionary<string, string> InsertionRules) LoadData(string datafile)
