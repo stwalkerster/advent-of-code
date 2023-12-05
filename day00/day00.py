d00e8d83a4eec3917146f0dae6f9ebdb063a06e4
Simon Walker <github@stwalkerster.co.uk>
2023-12-05 19:47:47 +0000

    Add template day

---
+++ day00/day00.py
@@ -0,0 +1,11 @@
+def part1(inputFile):
+    input = open(inputFile).read().split('\n')
+
+
+def part2(inputFile):
+    input = open(inputFile).read().split('\n')
+
+
+if __name__ == '__main__':
+    part1("example.txt")
+
