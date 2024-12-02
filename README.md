180443ce7e6fa71a23a1c1987868355fa9dab07c
Simon Walker <github@stwalkerster.co.uk>
2024-12-02 16:05:16 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# [Advent of Code 2024](https://adventofcode.com/2024)
+
+My solutions to the Advent of Code challenges
---
+++ day00/main.py
@@ -0,0 +1,10 @@
+def part1(input_file):
+    data = [line for line in open(input_file).read().split('\n') if line != ""]
+
+
+def part2(input_file):
+    data = [line for line in open(input_file).read().split('\n') if line != ""]
+
+
+if __name__ == '__main__':
+    part1("example.txt")
