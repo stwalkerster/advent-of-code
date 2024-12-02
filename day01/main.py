a5d422e311a9d15a0431a65422998868b936b8bb
Simon Walker <github@stwalkerster.co.uk>
2024-12-02 16:22:51 +0000

    day1

---
+++ day01/main.py
@@ -0,0 +1,32 @@
+# from math import abs
+
+def part1(input_file):
+    data = [line.split('   ') for line in open(input_file).read().split('\n') if line != ""]
+
+    list1 = [int(x[0]) for x in data]
+    list1.sort()
+    list2 = [int(x[1]) for x in data]
+    list2.sort()
+
+    deltas = [abs(x - y) for x, y in zip(list1, list2)]
+
+    print(sum(deltas))
+
+
+def part2(input_file):
+    data = [line.split('   ') for line in open(input_file).read().split('\n') if line != ""]
+
+    list1 = [int(x[0]) for x in data]
+    list2 = [int(x[1]) for x in data]
+    list2.sort()
+
+    similarity = 0
+
+    for i in list1:
+        similarity += i * list2.count(i)
+
+
+    print(similarity)
+
+if __name__ == '__main__':
+    part2("input.txt")
