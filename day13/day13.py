ded1a3ae0fa3667c811c8e9a1754ce9bc2e553fc
Simon Walker <github@stwalkerster.co.uk>
2024-12-07 16:16:12 +0000

    day 13 part 2

--- day13/day13.py
+++ day13/day13.py
@@ -24,11 +24,9 @@ def part1(input_file):
     print("Total:", total)
 
 
-def test_reflection(pattern):
+def test_reflection(pattern, dir='z', skip=None):
     for y in range(1, len(pattern)):
         if pattern[y] == pattern[y - 1]:
-            print('   ', f"possible reflection at z={y}?")
-
             y_low = y - 2
             y_high = y + 1
 
@@ -37,14 +35,13 @@ def test_reflection(pattern):
 
                 if pattern[y_low] != pattern[y_high]:
                     matching = False
-                    print('   ', f"Reflection failed: {pattern[y_low]} != {pattern[y_high]}")
                     break
 
                 y_low -= 1
                 y_high += 1
 
-            if matching:
-                print('   ', f"Reflection confirmed!")
+            if matching and y != (skip or -1):
+                print('   ', f"Reflection confirmed at {dir}={y}!")
                 return y
     return None
 
@@ -58,6 +55,40 @@ def pretty_array(d):
 
 def part2(input_file):
     data = [line for line in open(input_file).read().split('\n\n') if line != ""]
+
+    total = 0
+    for pattern_data in data:
+        print('New Pattern.')
+        pattern = [line for line in pattern_data.split('\n') if line != ""]
+
+        orig_row = test_reflection(pattern)
+        orig_col = test_reflection(list(zip(*pattern)))
+
+        exit_loop = False
+        for y in range(len(pattern)):
+            for x in range(len(pattern[0])):
+                local_pattern = pattern.copy()
+
+                modified_line = list(local_pattern[y])
+                modified_line[x] = '.' if modified_line[x] == '#' else '#'
+                local_pattern[y] = ''.join(modified_line)
+
+                row = test_reflection(local_pattern, dir='r', skip=orig_row)
+                if row is not None and (orig_row is None or row != orig_row):
+                    total += (100 * row)
+                    exit_loop = True
+                    break
+
+                col = test_reflection(list(zip(*local_pattern)), dir='c', skip=orig_col)
+                if col is not None and (orig_col is None or col != orig_col):
+                    total += col
+                    exit_loop = True
+                    break
+
+            if exit_loop:
+                break
+
+    print("Total:", total)
 
 
 if __name__ == '__main__':
