4ae9bdd2ff8af6568b361131723d7405474978ee
Simon Walker <github@stwalkerster.co.uk>
2024-12-24 19:13:29 +0000

    Refactor template for returns

--- day00/main.py
+++ day00/main.py
@@ -1,10 +1,13 @@
 def part1(input_file):
     data = [line for line in open(input_file).read().split('\n') if line != ""]
 
+    return None
+
 
 def part2(input_file):
     data = [line for line in open(input_file).read().split('\n') if line != ""]
 
+    return None
 
 if __name__ == '__main__':
-    part1("example.txt")
+    print(part1("example.txt"))
